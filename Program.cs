b96efdf5795dfd1ad993f886fd2309ccee204891
hubertwi1115 <148622055+hubertwi1115@users.noreply.github.com>
2023-11-04 14:51:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rozmiar;
+
+        rozmiar = sizeof(float);
+        Console.WriteLine($"Rozmiar typu float: {rozmiar} bajtów");
+
+        rozmiar = sizeof(char);
+        Console.WriteLine($"Rozmiar typu char: {rozmiar} bajtów");
+
+        rozmiar = sizeof(double);
+        Console.WriteLine($"Rozmiar typu double: {rozmiar} bajtów");
+
+        rozmiar = sizeof(int);
+        Console.WriteLine($"Rozmiar typu int: {rozmiar} bajtów");
+    }
+}
